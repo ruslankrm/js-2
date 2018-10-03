99be600b0295c4952f844edbf103df1d4b7d3a3b
ruslan <ruslankrm@bigmir.net>
2018-10-03 10:42:00 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -4,25 +4,27 @@ const a = parseFloat(prompt('number a'));
 const b = parseFloat(prompt('number b'));
 const c = parseFloat(prompt('number c'));
 
-function resultat(a, b, c){
+function resultat(a, b, c) {
     const d = discr(a, b, c);
+    if (d > 0) {
+        const x1 = (-b - Math.sqrt(d)) / (2 * a);
+        const x2 = (b - Math.sqrt(d)) / (2 * a);
+        return 'x1 = ' + x1 + ' x2 = ' + x2;
+    }
+    else if (d === 0) {
+        const x1 = (-b) / (2 * a);
+        return 'x = ' + x1;
+    }
+    else if (d < 0) {
+        return 'No results';
+    }
 }
 
-function discr(a, b, c){
-     const d = b*b-4*a*c;
-     
-     if(d > 0) {
-         const x1 = (-b + Math.sqrt(d))/(2*a);
-         const x2 = (-b + Math.sqrt(d))/(2*a);
-         alert('x1 = ' + x1 + ' x2 = ' + x2);
-     }else if (d === 0){
-         x1 = (-b)/(2*a);
-         alert('x = +x1')
-     }else (d < 0) {
-         return 'no solution';
-     }
+function discr(a, b, c) {
+    const d = b * b - 4 * a * c;
+    return d;
 }
 
 
-  
-   
+const solution = resultat(a, b, c);
+alert(solution);
